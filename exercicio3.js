7da417e574fe4d981670bcaff9b723b97b0e0710
Rodrigo Duarte Maciel <rods.dduarte@gmail.com>
2023-01-26 18:11:20 -0300

    exercicio 3

---
+++ exercicio3.js
@@ -0,0 +1,158 @@
+const data = [
+	{
+		"dia": 1,
+		"valor": 22174.1664
+	},
+	{
+		"dia": 2,
+		"valor": 24537.6698
+	},
+	{
+		"dia": 3,
+		"valor": 26139.6134
+	},
+	{
+		"dia": 4,
+		"valor": 0.0
+	},
+	{
+		"dia": 5,
+		"valor": 0.0
+	},
+	{
+		"dia": 6,
+		"valor": 26742.6612
+	},
+	{
+		"dia": 7,
+		"valor": 0.0
+	},
+	{
+		"dia": 8,
+		"valor": 42889.2258
+	},
+	{
+		"dia": 9,
+		"valor": 46251.174
+	},
+	{
+		"dia": 10,
+		"valor": 11191.4722
+	},
+	{
+		"dia": 11,
+		"valor": 0.0
+	},
+	{
+		"dia": 12,
+		"valor": 0.0
+	},
+	{
+		"dia": 13,
+		"valor": 3847.4823
+	},
+	{
+		"dia": 14,
+		"valor": 373.7838
+	},
+	{
+		"dia": 15,
+		"valor": 2659.7563
+	},
+	{
+		"dia": 16,
+		"valor": 48924.2448
+	},
+	{
+		"dia": 17,
+		"valor": 18419.2614
+	},
+	{
+		"dia": 18,
+		"valor": 0.0
+	},
+	{
+		"dia": 19,
+		"valor": 0.0
+	},
+	{
+		"dia": 20,
+		"valor": 35240.1826
+	},
+	{
+		"dia": 21,
+		"valor": 43829.1667
+	},
+	{
+		"dia": 22,
+		"valor": 18235.6852
+	},
+	{
+		"dia": 23,
+		"valor": 4355.0662
+	},
+	{
+		"dia": 24,
+		"valor": 13327.1025
+	},
+	{
+		"dia": 25,
+		"valor": 0.0
+	},
+	{
+		"dia": 26,
+		"valor": 0.0
+	},
+	{
+		"dia": 27,
+		"valor": 25681.8318
+	},
+	{
+		"dia": 28,
+		"valor": 1718.1221
+	},
+	{
+		"dia": 29,
+		"valor": 13220.495
+	},
+	{
+		"dia": 30,
+		"valor": 8414.61
+	}
+]
+
+function billingInfo(data) {
+    let lowest = {dia: null, valor: Number.POSITIVE_INFINITY}
+    let biggest = {dia: null, valor: Number.NEGATIVE_INFINITY};
+    let validDays = 0;
+    let totalAmount = 0;
+
+    for(const day of data) {
+
+        if(day.valor === 0) continue;
+
+        
+        if(day.valor < lowest.valor) {
+            lowest = day;
+        }
+        
+        if(day.valor > biggest.valor) {
+            biggest = day;
+        }
+        
+        validDays++;
+        totalAmount += day.valor;
+    }
+
+    const mean = totalAmount/validDays;
+    let totalDays = 0;
+    for(const day of data) {
+        if(day.valor > mean) totalDays++; 
+    }
+
+    console.log(`Menor valor de faturamento foi de ${lowest.valor} reais no dia ${lowest.dia}.`);
+    console.log(`Maior valor de faturamento foi de ${biggest.valor} reais no dia ${biggest.dia}.`);
+    console.log(`Total de dias em que o faturamento foi maior que a media: ${totalDays}.`);
+}
+
+billingInfo(data);
